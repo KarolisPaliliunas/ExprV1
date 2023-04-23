21eed909e7b9198347bdd9ef71c269b4c2317e02
karol <karol@DESKTOP-2EN7OAL>
2023-04-23 21:06:27 +0300

    20230423-2

--- resources/views/es-tree-editor.blade.php
+++ resources/views/es-tree-editor.blade.php
@@ -1,34 +1,36 @@
 <x-wrapper-layout>
-    <form method="get" action="{{ route('project.list') }}">
+  <form method="get" action="{{ route('project.list') }}">
     @csrf
-        <button type="submit" class="btn btn-outline-danger form-control" style="width:auto; margin-left:90%">{{ __('BackToList') }}</button>
-    </form>
-    @isset ($project->id)
-      @isset ($projectTree)
-        <div class="border border-secondary overflow-auto" style="height:600px; width:100%; margin-top:20px">
-          <div class="tree">
-            @include('recursive', ['projectTree' => $projectTree, 'project_id' => $project->id])
-          </div>
-        </div>
-      @endisset
-      @empty ($projectTree)
-        <div class="border border-secondary overflow-auto" style="height:600px; width:100%; margin-top:20px">
-          <form method="post" action="{{ route('attribute.new', ['item_id'=>$project->id, 'project_id'=>$project->id, 'createForProject'=>true]) }}">
+    <button type="submit" class="btn btn-outline-danger form-control" style="width:auto; margin-left:90%">{{ __('BackToList') }}</button>
+  </form>
+  @isset ($project->id)
+  @isset ($projectTree)
+  <div class="border border-secondary overflow-auto" style="height:600px; width:100%; margin-top:20px">
+    <div class="tree">
+      @include('recursive', ['projectTree' => $projectTree, 'project_id' => $project->id])
+    </div>
+  </div>
+  @endisset
+  @empty ($projectTree)
+  <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+    <div class="py-12">
+        <form method="post" action="{{ route('attribute.new', ['item_id'=>$project->id, 'project_id'=>$project->id, 'createForProject'=>true]) }}">
           @csrf
-            <div>
-              <div class="row">
-                <label for="name1" class="form-label">{{ __('Your system is empty! Lets create a header attribute') }}</label>
-              </div>
-              <div class="row">
-                <input type="text" placeholder="{{ __('Main attribute name') }}" name="name">
-                <input type="text" placeholder="{{ __('Main attribute description') }}" name="description">
-              </div>
-              <div class="row">
-                <button type="submit" class="btn btn-outline-success form-control">{{ __('Create') }}</button>
-              </div>
+          <div>
+            <div class="row">
+              <label for="name1" class="form-label">{{ __('Your system is empty! Lets create a header attribute') }}</label>
             </div>
-          </form>
-        </div>
-      @endempty
-    @endisset
+            <div class="row">
+              <input type="text" placeholder="{{ __('Main attribute name') }}" name="name">
+              <input type="text" placeholder="{{ __('Main attribute description') }}" name="description">
+            </div>
+            <div class="row">
+              <button type="submit" class="btn btn-outline-success form-control">{{ __('Create') }}</button>
+            </div>
+          </div>
+        </form>
+    </div>
+  </div>
+  @endempty
+  @endisset
 </x-wrapper-layout>
--- resources/views/recursive.blade.php
+++ resources/views/recursive.blade.php
@@ -24,42 +24,24 @@ function traverseArray($DataArray, $project_id){
                 <li><a class="dropdown-item">';
                     switch($DataArray['type']){ 
                         case 10: //////////////////////////////////////ATTRIBUTE
-                        if($numberOfArrays == 0)
-                            echo '<form method="post" action='.route('value.new', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'>';
-                        else
-                            echo '<form method="post" action='.route('attribute.update', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'>';
-                echo   '<input type="hidden" name="_token" value="'.csrf_token().'" />       
-                        <div class="row">
-                        <div class="col">';
                         if($numberOfArrays == 0){
-                                        echo '
-                                        <div class="col">
-                                            <label for="name1" class="form-label">'.__('Create - Value').'</label>
-                                        </div>
-                                        <input type="text" placeholder="'.__('ItemNamePlaceholder').'" name="name">';
-                            }
-                            else {
-                                        echo '
-                                        <div class="col">
-                                            <label for="name1" class="form-label">'.__('Edit - Attribute').'</label>
-                                        </div>
-                                        <input type="text" value="'.$DataArray['name'].'" name="name">';
-                            }
-                    echo   '</div>
-                            <div class="col">';
-                            if($numberOfArrays == 0)
-                                echo '<input type="text" placeholder="'.__('ItemDescriptionPlaceholder').'" name="description">';
-                            else 
-                                echo '<input type="text" value="'.$DataArray['description'].'" name="description">';
-                    echo    '</div>
-                            <div class="col">';
-                            if($numberOfArrays == 0)
-                                echo '<button type="submit" class="btn btn-outline-success form-control">'.__('Create').'</button>';
-                            else
-                                echo '<button type="submit" class="btn btn-outline-success form-control">'.__('Edit').'</button>';    
-                    echo    '</div>
-                        </div>    
-                        </form>
+                            echo '<form method="post" action='.route('value.new', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'>
+                            <input type="hidden" name="_token" value="'.csrf_token().'" />       
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col">
+                                        <label for="name1" class="form-label">'.__('Create - Value').'</label>
+                                    </div>
+                                    <input type="text" placeholder="'.__('ItemNamePlaceholder').'" name="name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" placeholder="'.__('ItemDescriptionPlaceholder').'" name="description">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-outline-success form-control">'.__('Create').'</button>
+                                </div>
+                            </div>    
+                            </form>
                         </a></li>
                         <li>
                             <form method="post" action="'.route('attribute.delete', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
@@ -68,50 +50,74 @@ function traverseArray($DataArray, $project_id){
                             </form>
                         </li>
                         </ul>
-                        </div>
-                        ';
+                        </div>';
+                        } else{
+                            echo '<form method="post" action='.route('attribute.update', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'>
+                            <input type="hidden" name="_token" value="'.csrf_token().'" />       
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col">
+                                        <label for="name1" class="form-label">'.__('Edit - Attribute').'</label>
+                                    </div>
+                                    <input type="text" value="'.$DataArray['name'].'" name="name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" value="'.$DataArray['description'].'" name="description">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-outline-success form-control">'.__('Edit').'</button>
+                                </div>
+                            </div>
+                            </form>
+                            <form method="post" action='.route('value.new', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'>
+                            <input type="hidden" name="_token" value="'.csrf_token().'" />       
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col">
+                                        <label for="name1" class="form-label">'.__('Create - Value').'</label>
+                                    </div>
+                                    <input type="text" placeholder="'.__('ItemNamePlaceholder').'" name="name">
+                                </div>
+                                <div class="col">
+                                    <input type="text" placeholder="'.__('ItemDescriptionPlaceholder').'" name="description">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-outline-success form-control">'.__('Create').'</button>
+                                </div>
+                            </div>    
+                            </form>
+                        </a></li>
+                        <li>
+                            <form method="post" action="'.route('attribute.delete', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
+                            <input type="hidden" name="_token" value="'.csrf_token().'" />
+                                <a class="dropdown-item"><button type="submit"><i class="bi bi-trash"></i></button></a>
+                            </form>
+                        </li>
+                        </ul>
+                        </div>';
+                        }
                         break;
                         case 20: ///////////////////////////VALUE
                         if($numberOfArrays == 0){
-                            echo '<form method="post" action="'.route('conclusion.new', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">';
-                        }
-                        else
-                            echo '<form method="post" action="'.route('value.update', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">';
-                echo   '<input type="hidden" name="_token" value="'.csrf_token().'" /> 
-                        <div class="row">
-                        <div class="col">';
-                        if($numberOfArrays == 0){
-                                        echo '
-                                        <div class="col">
-                                            <label for="name1" class="form-label">'.__('Create - Conclusion').'</label>
-                                        </div>
-                                        <input type="text" placeholder="'.__('ItemNamePlaceholder').'" name="name">';
-                            }
-                            else {
-                                        echo '
-                                        <div class="col">
-                                            <label for="name1" class="form-label">'.__('Edit - Value').'</label>
-                                        </div>
-                                        <input type="text" value="'.$DataArray['name'].'" name="name">';
-                            }
-                    echo   '</div>
-                            <div class="col">';
-                            if($numberOfArrays == 0)
-                                echo '<input type="text" placeholder="'.__('ItemDescriptionPlaceholder').'" name="description">';
-                            else 
-                                echo '<input type="text" value="'.$DataArray['description'].'" name="description">';
-                    echo    '</div>
-                            <div class="col">';
-                            if($numberOfArrays == 0)
-                                echo '<button type="submit" class="btn btn-outline-success form-control">'.__('Create').'</button>';
-                            else
-                                echo '<button type="submit" class="btn btn-outline-success form-control">'.__('Edit').'</button>';    
-                    echo    '</div>
-                        </div>    
-                        </form>
-                        </a></li>';
-                        if ($numberOfArrays == 0)
-                echo    '<li>
+                            echo '<form method="post" action="'.route('conclusion.new', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
+                            <input type="hidden" name="_token" value="'.csrf_token().'" /> 
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col">
+                                        <label for="name1" class="form-label">'.__('Create - Conclusion').'</label>
+                                    </div>
+                                        <input type="text" placeholder="'.__('ItemNamePlaceholder').'" name="name">
+                                    </div>
+                                <div class="col">
+                                    <input type="text" placeholder="'.__('ItemDescriptionPlaceholder').'" name="description">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-outline-success form-control">'.__('Create').'</button>
+                                </div>
+                            </div>    
+                            </form>
+                            </a></li>
+                            <li>
                             <div class="dropdown-item">
                                 <form method="post" action="'.route('attribute.new', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id, 'createForProject'=>false]).'">
                                 <input type="hidden" name="_token" value="'.csrf_token().'" />
@@ -133,19 +139,47 @@ function traverseArray($DataArray, $project_id){
                                     </div>    
                                 </form>
                             </div>    
-                        </li>';
-                echo   '<li>
-                            <form method="post" action="'.route('value.delete', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
-                            <input type="hidden" name="_token" value="'.csrf_token().'" />
-                                <a class="dropdown-item"><button type="submit"><i class="bi bi-trash"></i></button></a>
-                            </form>
-                        </li>
+                            </li>
+                            <li>
+                                <form method="post" action="'.route('value.delete', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
+                                <input type="hidden" name="_token" value="'.csrf_token().'" />
+                                    <a class="dropdown-item"><button type="submit"><i class="bi bi-trash"></i></button></a>
+                                </form>
+                            </li>
                         </ul>
                         </div>
                         ';
+                        } else {
+                            echo '<form method="post" action="'.route('value.update', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
+                            <input type="hidden" name="_token" value="'.csrf_token().'" /> 
+                            <div class="row">
+                                <div class="col">
+                                    <div class="col">
+                                        <label for="name1" class="form-label">'.__('Edit - Value').'</label>
+                                    </div>
+                                        <input type="text" value="'.$DataArray['name'].'" name="name">
+                                    </div>
+                                <div class="col">
+                                    <input type="text" value="'.$DataArray['description'].'" name="description">
+                                </div>
+                                <div class="col">
+                                    <button type="submit" class="btn btn-outline-success form-control">'.__('Edit').'</button>
+                                </div>
+                            </div>    
+                            </form>
+                            </a></li>
+                            <li>
+                                <form method="post" action="'.route('value.delete', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
+                                <input type="hidden" name="_token" value="'.csrf_token().'" />
+                                    <a class="dropdown-item"><button type="submit"><i class="bi bi-trash"></i></button></a>
+                                </form>
+                            </li>
+                        </ul>
+                        </div>';
+                        }
                         break;
-                        case 30:
-                echo   '<form method="post" action="'.route('conclusion.update', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
+                        case 30:    
+                        echo   '<form method="post" action="'.route('conclusion.update', ['item_id'=>$DataArray['id'], 'project_id'=>$project_id]).'">
                         <input type="hidden" name="_token" value="'.csrf_token().'" /> 
                         <div class="row">
                             <div class="col">
